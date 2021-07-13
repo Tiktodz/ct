31f189ab6b039416fa30f4560ed0d5cf4c610559
kessaras <paul@cypheros.co>
2021-07-13 23:59:42 +0530

    sdm660-common: hals: Add call to build audio@4.0-service
    - Fix error:
    07-13 00:20:10.980     0     0 I init    : Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found
    
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -29,6 +29,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio4.0-service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service
 
