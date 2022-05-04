f0b0fc6e13a1fe1c2d9983f52c4893460449bc79
Tiktodz <kuplemarkeple@gmail.com>
2022-05-04 15:57:28 +0000

    Revert "sdm660-common: overlay: Add `xd_about_phone`"
    This reverts commit 6a41aa82e3e8f481a99e9e57bb64d58a21daa48b.

--- sdm660.mk
+++ sdm660.mk
@@ -282,8 +282,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xd
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
