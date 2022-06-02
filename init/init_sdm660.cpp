af23a3bf6295f657e8d22ffded689236e257a37d
Tiktodz <ewprjkt@proton.me>
2022-06-02 20:39:07 +0000

    sdm660-common: Drop fingerprint override
    Change-Id: Ibd36aa5dd5dc4129d27f00bf3d78d434f4c8efc4

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -129,8 +129,5 @@ void vendor_load_properties()
     property_override("ro.boot.selinux", "enforcing");
     property_override("ro.boot.veritymode", "enforcing");
     property_override("ro.boot.vbmeta.device_state", "locked");
-    property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-    property_override("ro.system.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 
 }
